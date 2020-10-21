4fc7e796874983955704dc47b7fba43682a1e0f3
mahesh-aerra <aerramahesh@gmail.com>
2020-10-21 19:01:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace visualstudiocsharpgitdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
